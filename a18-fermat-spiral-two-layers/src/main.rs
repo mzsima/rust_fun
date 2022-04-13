a858b4db6cde570778110b03c960916082ca38f6
yusuke mizushima <mzsima@gmail.com>
2022-04-13 22:47:38 +0900

    spiral two layers

---
+++ a18-fermat-spiral-two-layers/src/main.rs
@@ -0,0 +1,43 @@
+use nannou::glam::*;
+use nannou::prelude::*;
+
+struct Model {
+    draw: Draw,
+}
+
+fn main() {
+    nannou::app(model)
+        .loop_mode(LoopMode::loop_once())
+        .update(update)
+        .run();
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
+    let draw = Draw::new();
+    draw.background().color(BLACK);
+    Model { draw }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    setup(&_model);
+    _model.draw.to_frame(app, &frame).unwrap();
+}
+
+fn setup(model: &Model) {
+    draw_dot(&model, 0, 72.0 / 305.0, DEEPPINK);
+    draw_dot(&model, 0, 17.0 / 72.0, ROYALBLUE);
+}
+
+fn draw_dot(model: &Model, i: u32, rot: f32, color: Srgb<u8>) {
+    if i > 1650 {
+        return;
+    }
+    let r = 4. * (i as f32).sqrt();
+    let angle = (i as f32) * rot * 2. * PI;
+    let (x, y) = (r * angle.sin(), r * angle.cos());
+    model.draw.ellipse().w_h(4., 4.).x_y(x, y).color(color);
+    draw_dot(&model, i + 1, rot, color);
+}
